f93ef3a63ea8ced4d7829d1ca59757a14d262752
YasirBinQaiser <Yasir.Qaiser@bentley.com>
2023-02-23 17:53:49 +0500

    Initial Revision

---
+++ OneNoteLauncher/OneNoteLauncher/OneNoteSearchData.cs
@@ -0,0 +1,371 @@
+﻿
+// NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+/// <remarks/>
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.microsoft.com/office/onenote/2013/onenote")]
+[System.Xml.Serialization.XmlRootAttribute(Namespace = "http://schemas.microsoft.com/office/onenote/2013/onenote", IsNullable = false)]
+public partial class Notebook
+{
+
+    private NotebookSection sectionField;
+
+    private string nameField;
+
+    private string nicknameField;
+
+    private string idField;
+
+    private string pathField;
+
+    private System.DateTime lastModifiedTimeField;
+
+    private string colorField;
+
+    private bool isCurrentlyViewedField;
+
+    /// <remarks/>
+    public NotebookSection Section
+    {
+        get
+        {
+            return this.sectionField;
+        }
+        set
+        {
+            this.sectionField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string name
+    {
+        get
+        {
+            return this.nameField;
+        }
+        set
+        {
+            this.nameField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string nickname
+    {
+        get
+        {
+            return this.nicknameField;
+        }
+        set
+        {
+            this.nicknameField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string ID
+    {
+        get
+        {
+            return this.idField;
+        }
+        set
+        {
+            this.idField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string path
+    {
+        get
+        {
+            return this.pathField;
+        }
+        set
+        {
+            this.pathField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public System.DateTime lastModifiedTime
+    {
+        get
+        {
+            return this.lastModifiedTimeField;
+        }
+        set
+        {
+            this.lastModifiedTimeField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string color
+    {
+        get
+        {
+            return this.colorField;
+        }
+        set
+        {
+            this.colorField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public bool isCurrentlyViewed
+    {
+        get
+        {
+            return this.isCurrentlyViewedField;
+        }
+        set
+        {
+            this.isCurrentlyViewedField = value;
+        }
+    }
+}
+
+/// <remarks/>
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.microsoft.com/office/onenote/2013/onenote")]
+public partial class NotebookSection
+{
+
+    private NotebookSectionPage pageField;
+
+    private string nameField;
+
+    private string idField;
+
+    private string pathField;
+
+    private System.DateTime lastModifiedTimeField;
+
+    private string colorField;
+
+    private bool isCurrentlyViewedField;
+
+    /// <remarks/>
+    public NotebookSectionPage Page
+    {
+        get
+        {
+            return this.pageField;
+        }
+        set
+        {
+            this.pageField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string name
+    {
+        get
+        {
+            return this.nameField;
+        }
+        set
+        {
+            this.nameField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string ID
+    {
+        get
+        {
+            return this.idField;
+        }
+        set
+        {
+            this.idField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string path
+    {
+        get
+        {
+            return this.pathField;
+        }
+        set
+        {
+            this.pathField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public System.DateTime lastModifiedTime
+    {
+        get
+        {
+            return this.lastModifiedTimeField;
+        }
+        set
+        {
+            this.lastModifiedTimeField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string color
+    {
+        get
+        {
+            return this.colorField;
+        }
+        set
+        {
+            this.colorField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public bool isCurrentlyViewed
+    {
+        get
+        {
+            return this.isCurrentlyViewedField;
+        }
+        set
+        {
+            this.isCurrentlyViewedField = value;
+        }
+    }
+}
+
+/// <remarks/>
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.microsoft.com/office/onenote/2013/onenote")]
+public partial class NotebookSectionPage
+{
+
+    private string idField;
+
+    private string nameField;
+
+    private System.DateTime dateTimeField;
+
+    private System.DateTime lastModifiedTimeField;
+
+    private byte pageLevelField;
+
+    private bool isCurrentlyViewedField;
+
+    private string selectedField;
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string ID
+    {
+        get
+        {
+            return this.idField;
+        }
+        set
+        {
+            this.idField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string name
+    {
+        get
+        {
+            return this.nameField;
+        }
+        set
+        {
+            this.nameField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public System.DateTime dateTime
+    {
+        get
+        {
+            return this.dateTimeField;
+        }
+        set
+        {
+            this.dateTimeField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public System.DateTime lastModifiedTime
+    {
+        get
+        {
+            return this.lastModifiedTimeField;
+        }
+        set
+        {
+            this.lastModifiedTimeField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public byte pageLevel
+    {
+        get
+        {
+            return this.pageLevelField;
+        }
+        set
+        {
+            this.pageLevelField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public bool isCurrentlyViewed
+    {
+        get
+        {
+            return this.isCurrentlyViewedField;
+        }
+        set
+        {
+            this.isCurrentlyViewedField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string selected
+    {
+        get
+        {
+            return this.selectedField;
+        }
+        set
+        {
+            this.selectedField = value;
+        }
+    }
+}
+
---
+++ OneNoteLauncher/OneNoteLauncher/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data;
+
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Win32;
+
+using Microsoft.Office.Interop.OneNote;
+
+
+namespace OneNoteLauncher
+{
+static class Program
+{
+static void Main(string[] args)
+    {
+    string strs = "";
+    foreach (string str in args)
+        strs = strs + " " + str;
+    
+    strs = strs.Trim(); 
+    LaunchOneNote (strs);
+    }
+
+static string GetOneNoteRoot()
+    {
+    string rootFolder = "";
+
+    string registry_key = @"SOFTWARE\Microsoft\Office\";
+    using (Microsoft.Win32.RegistryKey key = Registry.LocalMachine.OpenSubKey(registry_key))
+        {
+        foreach (string subkey_name in key.GetSubKeyNames())
+            {
+            double val = 0.0;
+            if (!double.TryParse(subkey_name, out val))
+                continue;
+
+            RegistryKey subkey = Registry.LocalMachine.OpenSubKey(registry_key + subkey_name + "\\onenote\\installroot\\");
+            if (subkey == null)
+                continue;
+
+            rootFolder = subkey.GetValue ("Path") as string;
+            break;
+
+            }
+        }
+
+
+    return rootFolder;
+    }
+
+
+//string oneNoteURL = "onenote:///\"" + notebook.Section.path +
+//                                    "\"#\"" + notebook.Section.Page.name +
+//                                    "\"&section-id=" + notebook.Section.ID +
+//                                    "&page-id=" + notebook.Section.Page.ID +
+//                                    "&end";
+
+//exeProcess.StartInfo.Arguments = "/hyperlink " + oneNoteURL;
+
+static void LaunchOneNote(string filter)
+    {
+    Microsoft.Office.Interop.OneNote.Application oneNoteApp = new Microsoft.Office.Interop.OneNote.Application();
+            
+    string noteBookXML;
+    oneNoteApp.GetHierarchy(null, HierarchyScope.hsNotebooks, out noteBookXML);
+    XDocument xDocument = XDocument.Parse(noteBookXML);
+    XNamespace ns = xDocument.Root.Name.Namespace;
+ 
+    int iterator = 0;
+    foreach (XElement item in from node in xDocument.Descendants(ns + "Notebook") select (node))
+        {
+        string id = item.Attribute("ID").Value;
+        try
+            {
+            string searchResult = "";
+            ((IApplication)oneNoteApp).FindPages(id, filter, out searchResult);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Notebook));
+            TextReader reader = new StringReader(searchResult);
+            Notebook notebook = xmlSerializer.Deserialize(reader) as Notebook;
+            if (notebook.Section != null && notebook.Section.Page != null)
+                {
+                if (oneNoteApp.Windows == null)
+                    {
+
+                    string oneNotePath = GetOneNoteRoot() + "OneNote.exe" ;
+
+                    if (string.IsNullOrEmpty(oneNotePath))
+                        return;
+
+                    Process exeProcess = new Process();
+                    exeProcess.StartInfo.FileName = oneNotePath;
+                    exeProcess.StartInfo.UseShellExecute = false;
+                    exeProcess.StartInfo.Arguments = "/nologo";
+                    exeProcess.Start();
+                    }
+
+                oneNoteApp.NavigateTo (notebook.Section.ID, notebook.Section.Page.ID);
+                return;
+                }
+            }
+        catch (Exception ex)
+            {
+            Console.WriteLine(ex.Message);
+            }
+        iterator++;
+        }
+
+    }
+}
+}
